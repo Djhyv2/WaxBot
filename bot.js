3bb361df2e4c963d160a63ae17ab0d237d228949
Dustin Hengel <djhyv2@mail.missouri.edu>
2019-12-16 19:08:25 -0600

    Made code nicer

--- bot.js
+++ bot.js
@@ -103,6 +103,154 @@ function printSlot(slot)
     return string;
 }//Converts array of rune and wax into string
 
+function parseMessage(message)
+{
+    let parsedValues = message.content.split('\n');
+    if (4 !== parsedValues.length)
+    {
+        return null;
+    }//If message not 4 lines, return
+
+
+    parsedValues = parsedValues.map((row) => row.split(',').map((part) =>
+    {
+        const components = part.replace(/ +/g, ' ').trim().split(' ');
+        if (2 !== components.length)
+        {
+            throw new Error('Invalid Format - Are you missing any commas or spaces?');
+        }
+        return {
+            rune: components[0].toLowerCase(),
+            wax: components[1],
+        };
+    }));//Splits multiline string into array of rows of comma seperated values, splits the comma seperated values into values, splits the values into 2 parts, rune and wax
+    return parsedValues;
+}
+
+function calculateAlts(parsedData, data)
+{
+    let computedData;
+
+    computedData = parsedData.map((row) => row.map((item) =>
+    {
+        if (null == data[item.rune])
+        {
+            throw new Error('Invalid Format - Did you misspell a rune?');
+        }
+        //eslint-disable-next-line no-param-reassign
+        item.profit = item.wax * data.wax.price - data[item.rune].price * data[item.rune].quantity;
+        //eslint-disable-next-line no-param-reassign
+        item.ironProfit = item.wax * data.wax.price - data[item.rune].ironPrice * data[item.rune].quantity;
+        return item;
+    }));//Calculates profit for given rune and quantity
+
+    //const mainRunes = [computedData[0][0], null, computedData[1][0], computedData[2][0], computedData[3][0]];//The runes for optimal number of wax, The null is in the spot for a secondary main rune
+
+    computedData = computedData.map((row) => row.sort((component1, component2) =>
+    {
+        if (component1.ironProfit > component2.ironProfit)
+        {
+            return -1;
+        }
+        if (component1.ironProfit < component2.ironProfit)
+        {
+            return 1;
+        }
+        return 0;
+    }));//Sorts each row by ironman profit
+
+    const ironAlts = [computedData[0][0], null, computedData[1][0], computedData[2][0], computedData[3][0]];//The runes for optimal amount of gold for ironmen
+    let secondSlotBetterCounter = 0;//Counts how many times second slot overrides 1
+    for (let slot = 1; 4 > slot; slot += 1)
+    {
+        if (computedData[0][0].rune === computedData[slot][0].rune)
+        {
+            if (1 < computedData[slot].length && (2 > computedData[0].length || computedData[0][1].ironProfit - computedData[0][0].ironProfit < computedData[slot][1].ironProfit - computedData[slot][0].ironProfit))
+            {
+                ironAlts[slot + 1] = computedData[slot][1];
+            }//If first slot switching loses more profit or there is no alternate first slot and there is alternate second slot use secondary second slot
+            else if (1 < computedData[0].length)
+            {
+                ironAlts[1] = computedData[0][1];
+                secondSlotBetterCounter += 1;
+            }//If second switching loses more profit and there is alternate first slot add alternate first slot
+        }//If conflicting rune
+    }//Checks for conflicts
+
+    if (3 === secondSlotBetterCounter)
+    {
+        ironAlts[0] = computedData[0][1];
+        ironAlts[1] = null;
+    }//If second slot has a better rune than the first for all 3, move the first slot to its alternate
+
+    computedData = computedData.map((row) => row.sort((component1, component2) =>
+    {
+        if (component1.profit > component2.profit)
+        {
+            return -1;
+        }
+        if (component1.profit < component2.profit)
+        {
+            return 1;
+        }
+        return 0;
+    }));//Sorts each row by profit
+
+    const alts = [computedData[0][0], null, computedData[1][0], computedData[2][0], computedData[3][0]];//The runes for optimal amount of gold
+    secondSlotBetterCounter = 0;//Counts how many times second slot overrides 1
+    for (let slot = 1; 4 > slot; slot += 1)
+    {
+        if (computedData[0][0].rune === computedData[slot][0].rune)
+        {
+            if (1 < computedData[slot].length && (2 > computedData[0].length || computedData[0][1].profit - computedData[0][0].profit < computedData[slot][1].profit - computedData[slot][0].profit))
+            {
+                alts[slot + 1] = computedData[slot][1];
+            }//If first slot switching loses more profit and there is alternate second slot use secondary second slot
+            else if (1 < computedData[0].length)
+            {
+                alts[1] = computedData[0][1];
+                secondSlotBetterCounter += 1;
+            }//If second switching loses more profit and there is alternate first slot add alternate first slot
+        }//If conflicting rune
+    }//Checks for conflicts
+
+    if (3 === secondSlotBetterCounter)
+    {
+        alts[0] = computedData[0][1];
+        alts[1] = null;
+    }//If second slot has a better rune than the first for all 3, move the first slot to its alternate
+
+    computedData = computedData.map((row) => row.sort((component1, component2) =>
+    {
+        if ('30' === component2.wax)
+        {
+            return 1;
+        }
+        if (component1.profit > component2.profit || '30' === component1.wax)
+        {
+            return -1;
+        }
+        if (component1.profit < component2.profit)
+        {
+            return 1;
+        }
+        return 0;
+    }));//Sorts each row by main rune then profit
+
+    const date = new Date().toLocaleDateString(
+        'en-US',
+        {
+            timeZone: 'UTC',
+            month: 'short',
+            day: '2-digit',
+            year: 'numeric',
+        },
+    );//Gets current date
+
+    const responseMessage = `${date}\nSlot 1:\n\t ${printSlot(computedData[0])}\nSlot 2:\n\t${printSlot(computedData[1])}\n\t${printSlot(computedData[2])}\n\t${printSlot(computedData[3])}\nAlts: ${capitalizeFirst(alts[0].rune)}${null != alts[1] ? `/${capitalizeFirst(alts[1].rune)}` : ''} ${capitalizeFirst(alts[2].rune)} ${capitalizeFirst(alts[3].rune)} ${capitalizeFirst(alts[4].rune)} \nIron Alts: ${capitalizeFirst(ironAlts[0].rune)}${null != ironAlts[1] ? `/${capitalizeFirst(ironAlts[1].rune)}` : ''} ${capitalizeFirst(ironAlts[2].rune)} ${capitalizeFirst(ironAlts[3].rune)} ${capitalizeFirst(ironAlts[4].rune)} `;//Creates readable version of alts
+    return responseMessage;
+}
+
 async function runBot()
 {
     const bot = new discord.Client();
@@ -124,165 +272,38 @@ async function runBot()
                 return;
             }//If message in wrong channel return
 
-            let computedValues = message.content.split('\n');
-            if (4 !== computedValues.length)
-            {
-                return;
-            }//If message not 4 lines, return
-
+            let parsedValues;
             try
             {
-                computedValues = computedValues.map((row) => row.split(',').map((part) =>
-                {
-                    const components = part.replace(/ +/g, ' ').trim().split(' ');
-                    if (2 !== components.length)
-                    {
-                        throw new Error('Invalid Format - Are you missing any commas or spaces?');
-                    }
-                    return {
-                        rune: components[0].toLowerCase(),
-                        wax: components[1],
-                    };
-                }));//Splits multiline string into array of rows of comma seperated values, splits the comma seperated values into values, splits the values into 2 parts, rune and wax
+                parsedValues = parseMessage(message);
             }
             catch (error)
             {
                 message.channel.send(error.message);
                 return;
-            }
+            }//Parse Message
+            if (null === parsedValues)
+            {
+                return;
+            }//Return if nothing parsed
+
 
             const data = await fetchGEPrices();
             if (null == data)
             {
                 message.channel.send('Unable to reach GE Server');
                 return;
-            }
+            }//Get Data from API
 
             try
             {
-                computedValues = computedValues.map((row) => row.map((item) =>
-                {
-                    if (null == data[item.rune])
-                    {
-                        throw new Error('Invalid Format - Did you misspell a rune?');
-                    }
-                    //eslint-disable-next-line no-param-reassign
-                    item.profit = item.wax * data.wax.price - data[item.rune].price * data[item.rune].quantity;
-                    //eslint-disable-next-line no-param-reassign
-                    item.ironProfit = item.wax * data.wax.price - data[item.rune].ironPrice * data[item.rune].quantity;
-                    return item;
-                }));//Calculates profit for given rune and quantity
-            }
+                message.channel.send(calculateAlts(parsedValues, data));
+            }//Calculate alts and send as message
             catch (error)
             {
                 message.channel.send(error.message);
                 return;
             }
-
-            const mainRunes = [computedValues[0][0], null, computedValues[1][0], computedValues[2][0], computedValues[3][0]];//The runes for optimal number of wax, The null is in the spot for a secondary main rune
-
-            computedValues = computedValues.map((row) => row.sort((component1, component2) =>
-            {
-                if (component1.ironProfit > component2.ironProfit)
-                {
-                    return -1;
-                }
-                if (component1.ironProfit < component2.ironProfit)
-                {
-                    return 1;
-                }
-                return 0;
-            }));//Sorts each row by ironman profit
-
-            const ironAlts = [computedValues[0][0], null, computedValues[1][0], computedValues[2][0], computedValues[3][0]];//The runes for optimal amount of gold for ironmen
-            let secondSlotBetterCounter = 0;//Counts how many times second slot overrides 1
-            for (let slot = 1; 4 > slot; slot += 1)
-            {
-                if (computedValues[0][0].rune === computedValues[slot][0].rune)
-                {
-                    if (1 < computedValues[slot].length && (2 > computedValues[0].length || computedValues[0][1].ironProfit - computedValues[0][0].ironProfit < computedValues[slot][1].ironProfit - computedValues[slot][0].ironProfit))
-                    {
-                        ironAlts[slot + 1] = computedValues[slot][1];
-                    }//If first slot switching loses more profit or there is no alternate first slot and there is alternate second slot use secondary second slot
-                    else if (1 < computedValues[0].length)
-                    {
-                        ironAlts[1] = computedValues[0][1];
-                        secondSlotBetterCounter += 1;
-                    }//If second switching loses more profit and there is alternate first slot add alternate first slot
-                }//If conflicting rune
-            }//Checks for conflicts
-
-            if (3 === secondSlotBetterCounter)
-            {
-                ironAlts[0] = computedValues[0][1];
-                ironAlts[1] = null;
-            }//If second slot has a better rune than the first for all 3, move the first slot to its alternate
-
-            computedValues = computedValues.map((row) => row.sort((component1, component2) =>
-            {
-                if (component1.profit > component2.profit)
-                {
-                    return -1;
-                }
-                if (component1.profit < component2.profit)
-                {
-                    return 1;
-                }
-                return 0;
-            }));//Sorts each row by profit
-
-            const alts = [computedValues[0][0], null, computedValues[1][0], computedValues[2][0], computedValues[3][0]];//The runes for optimal amount of gold
-            secondSlotBetterCounter = 0;//Counts how many times second slot overrides 1
-            for (let slot = 1; 4 > slot; slot += 1)
-            {
-                if (computedValues[0][0].rune === computedValues[slot][0].rune)
-                {
-                    if (1 < computedValues[slot].length && (2 > computedValues[0].length || computedValues[0][1].profit - computedValues[0][0].profit < computedValues[slot][1].profit - computedValues[slot][0].profit))
-                    {
-                        alts[slot + 1] = computedValues[slot][1];
-                    }//If first slot switching loses more profit and there is alternate second slot use secondary second slot
-                    else if (1 < computedValues[0].length)
-                    {
-                        alts[1] = computedValues[0][1];
-                        secondSlotBetterCounter += 1;
-                    }//If second switching loses more profit and there is alternate first slot add alternate first slot
-                }//If conflicting rune
-            }//Checks for conflicts
-
-            if (3 === secondSlotBetterCounter)
-            {
-                alts[0] = computedValues[0][1];
-                alts[1] = null;
-            }//If second slot has a better rune than the first for all 3, move the first slot to its alternate
-
-            computedValues = computedValues.map((row) => row.sort((component1, component2) =>
-            {
-                if ('30' === component2.wax)
-                {
-                    return 1;
-                }
-                if (component1.profit > component2.profit || '30' === component1.wax)
-                {
-                    return -1;
-                }
-                if (component1.profit < component2.profit)
-                {
-                    return 1;
-                }
-                return 0;
-            }));//Sorts each row by main rune then profit
-
-            const date = new Date().toLocaleDateString(
-                'en-US',
-                {
-                    timeZone: 'UTC',
-                    month: 'short',
-                    day: '2-digit',
-                    year: 'numeric',
-                },
-            );//Gets current date
-            const response = `${date}\nSlot 1:\n\t ${printSlot(computedValues[0])}\nSlot 2:\n\t${printSlot(computedValues[1])}\n\t${printSlot(computedValues[2])}\n\t${printSlot(computedValues[3])}\nAlts: ${capitalizeFirst(alts[0].rune)}${null != alts[1] ? `/${capitalizeFirst(alts[1].rune)}` : ''} ${capitalizeFirst(alts[2].rune)} ${capitalizeFirst(alts[3].rune)} ${capitalizeFirst(alts[4].rune)} \nIron Alts: ${capitalizeFirst(ironAlts[0].rune)}${null != ironAlts[1] ? `/${capitalizeFirst(ironAlts[1].rune)}` : ''} ${capitalizeFirst(ironAlts[2].rune)} ${capitalizeFirst(ironAlts[3].rune)} ${capitalizeFirst(ironAlts[4].rune)} `;//Creates readable version of alts
-            message.channel.send(response);
         },
     );//Message handler
 
